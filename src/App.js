572334f1ead6e1e8c517090c72d180e2565d9f61
YOUR NAME <YOUR EMAIL ADDRESS>
2020-07-19 08:57:22 -0700

    set up react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
-function App() {
+function app() {
   return (
-    <div className="App">
-        <h1>Amazon Clone!</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <switch>
+          <Route path="/checkout">
+            <h1>Checkout</h1>
+          </Route>
+          <Route path="/Login">
+            <h1>Log Page</h1>
+          </Route>
+          <Route path="/">
+            <h1>HOME PAGE!!!</h1>
+          </Route>
+        </switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+export default app;
+
+
